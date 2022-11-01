8919b7cab4dfd90313eb2d59bf3e531806d2b863
Kazuto-neves <chrismneves@gmail.com>
2022-11-01 15:27:11 -0300

    att arrumado

--- Js/script.js
+++ Js/script.js
@@ -5,77 +5,77 @@ const PASS = document.getElementById("password");
 const TBODY = document.getElementById("tbody");
 
 /*
-	aluno ou professor
+    aluno ou professor
 */
+
+async function span(msg){
+    let span = document.getElementById("checkExist");
+    span.innerText=msg;
+}
+
 const CONSULTAR = async () => {
-    let tipo = document.getElementById("checkExist");
-    tipo.innerText="Buscando...";
-    console.log("1");
-    let rota = LOGIN.value===""?(RPROF + "0"):(RPROF+LOGIN.value);
-    console.log("2");
-    try {
-        const DATA = await fetch(rota);
-        const JSON = await DATA.json();
-        tipo.innerText="professor";
-        console.log("3");
-        if(JSON[0].senha===PASS.value){
-        	console.log("logado como professor");
-        }else{
-		console.log("senha invalida");
-	}
-	console.log("4");
-        return JSON[0].id_professor;
-    } catch (error) {
-        console.log("5");
-        rota = LOGIN.value===""?(RALUN + "0"):(RALUN+LOGIN.value);
-        try {
-            console.log("6");
-            const DATA = await fetch(rota);
-            const JSON = await DATA.json();
-            console.log("7");
-            tipo.innerText="aluno";
-            if(JSON[0].senha===PASS.value){
-        	console.log("logado como aluno");
-            }else{
-		console.log("senha invalida");
-	    }
-	    console.log("9");
-            return JSON[0].id_aluno;
-        } catch (error) {
-            console.log("10");
-            console.log(error.message);
-        }
+    let rota = "";
+    let tipo = 0;
+    if (LOGIN.value.length == 1) {
+        rota = (RALUN + LOGIN.value);
+        tipo = 2;
+    } else if (LOGIN.value.length == 9) {
+        rota = (RPROF + LOGIN.value);
+        tipo = 1;
     }
-};
+    rota.length != 0 ? await clogin(tipo, rota) : span("erro");
+}
+
+async function clogin(tipo, rota) {
+    const DATA = await fetch(rota);
+    const JSON = await DATA.json();
+    if (tipo === 1) {
+        span("professor");
+        if (JSON[0].senha === PASS.value) {
+            console.log("logado como professor");
+        } else {
+            console.log("senha invalida");
+        }
+        return JSON[0].id_professor;
+    }else{
+        span("aluno");
+        if (JSON[0].senha === PASS.value) {
+            console.log("logado como aluno");
+        } else {
+            console.log("senha invalida");
+        }
+        return JSON[0].id_aluno;
+    }
+}
 
 async function listar() {
-	const DATA = await fetch(RPROF);
-        const JSON = await DATA.json();
-	console.log('click');
-	event.preventDefault();
-	for(let i=0;i<JSON.length;i++){
-	const TR = document.createElement('tr');
-	TR.classList.add('row');
-	const IDD = document.createElement('td');
-	IDD.innerText=JSON[0].id_professor;
-	const NOMED = document.createElement('td');
-	NOMED.innerText=JSON[i].nome;
-	const DATAD = document.createElement('td');
-	DATAD.innerText=JSON[i].data_nascimento;
-	const EMAILD = document.createElement('td');
-	EMAILD.innerText=JSON[i].email;
-	const SENHAD = document.createElement('td');
-	SENHAD.innerText=JSON[i].senha;
-	
-	TR.appendChild(IDD);
-	TR.appendChild(NOMED);
-	TR.appendChild(DATAD);
-	TR.appendChild(EMAILD);
-	TR.appendChild(SENHAD);
-	
-	TBODY.appendChild(TR);
-	};
-	
+    const DATA = await fetch(RPROF);
+    const JSON = await DATA.json();
+    console.log('click');
+    event.preventDefault();
+    for (let i = 0; i < JSON.length; i++) {
+        const TR = document.createElement('tr');
+        TR.classList.add('row');
+        const IDD = document.createElement('td');
+        IDD.innerText = JSON[0].id_professor;
+        const NOMED = document.createElement('td');
+        NOMED.innerText = JSON[i].nome;
+        const DATAD = document.createElement('td');
+        DATAD.innerText = JSON[i].data_nascimento;
+        const EMAILD = document.createElement('td');
+        EMAILD.innerText = JSON[i].email;
+        const SENHAD = document.createElement('td');
+        SENHAD.innerText = JSON[i].senha;
+
+        TR.appendChild(IDD);
+        TR.appendChild(NOMED);
+        TR.appendChild(DATAD);
+        TR.appendChild(EMAILD);
+        TR.appendChild(SENHAD);
+
+        TBODY.appendChild(TR);
+    };
+
 }
 
 
