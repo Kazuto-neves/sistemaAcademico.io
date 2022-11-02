d4c280b58ad1beb2c73ee8560b595897b1343ccf
Kazuto-neves <chrismneves@gmail.com>
2022-11-02 14:53:10 -0300

    insert

--- Js/swegerr.js
+++ Js/swegerr.js
@@ -1,46 +1,23 @@
 const RPROF = "http://localhost:3000/professores/consultar/";
 const RALUN = "http://localhost:3000/alunos/consultar/";
 const TBODY = document.getElementById("tbody");
-const FORM = document.getElementById("form");
 
-async function CarregarFormProf() {
-    event.preventDefault()
-    let url = "http://localhost:3000/professores/inserir";
-    let id = document.getElementById("id").value;
-    let nome = document.getElementById("nome").value;
-    let data = document.getElementById("data").value;
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
-    console.log(nome)
-    console.log(email)
-
-    body = {
-        "id_aluno": id,
-        "name": nome,
-        "data_nascimento": data,
-        "email": email,
-        "senha": senha
-    }
-
-    await inserir(url, body);
-}
-
-async function inserir(url, body) {
-    console.log("Body=", body);
-    let request = new XMLHttpRequest();
-    request.open("POST", url, true);
-    request.setRequestHeader("Content-type", "application/json");
-    request.onload = function () {
-        console.log(this.responseText);
-        console.log(request.status);
-    }
-    if (request.readyState === 4) {
-        if (request.status === 200) {
-            request.send(JSON.stringify(body))
-        }
-    }
-
-    return request.responseText
+async function listar() {
+    const DATA = await fetch(RPROF);
+    const JSON = await DATA.json();
+    console.log('click');
+    // var dados = JSON.parse(RPROF);
+    JSON.forEach(function (allalunos) {
+        var row =
+            `<tr> 
+                <td>${allalunos.id_professor}</td>
+                <td>${allalunos.nome}</td>
+                <td>${allalunos.data_nascimento}</td>
+                <td>${allalunos.email}</td>
+                <td>${allalunos.senha}</td>
+            </tr>`
+        TBODY.innerHTML += row;
+    });
 }
 
 FORM.addEventListener("submit", CarregarFormProf);
--- Js/util.js
+++
@@ -1,5 +0,0 @@
-const RPROF = "http://localhost:3000/professores/";
-const RALUN = "http://localhost:3000/alunos/";
-const ROUTS=[RALUN,RPROF];
-
-module.exports = ROUTS;
