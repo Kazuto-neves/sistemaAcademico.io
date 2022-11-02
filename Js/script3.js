cb1743874619e937ab8e8f02d8b13b62ee364c6a
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-11-02 15:58:17 -0300

    Create script3.js

---
+++ Js/script3.js
@@ -0,0 +1,31 @@
+const RPROF = "http://localhost:3000/professor/get-professor";
+const RALUN = "http://localhost:3000/aluno/consultar";
+const LOGIN = document.getElementById("button");
+const TBODY = document.getElementById("tbody");
+
+
+
+async function listar() {
+    const DATA = await fetch(RPROF);
+    const JSON = await DATA.json();
+    
+    JSON.forEach(function (allprofessor) {
+        var row =
+            `<tr> 
+                <td>${allprofessor.id_professor}</td>
+                <td>${allprofessor.nome}</td>
+                <td>${allprofessor.data_nascimento}</td>
+                <td>${allprofessor.email}</td>
+                <td>${allprofessor.senha}</td>
+            </tr>` 
+     TBODY.innerHTML += row;
+      console.log(row);
+
+    });
+}  
+
+const test = async () => {
+    await listar();
+};
+
+
